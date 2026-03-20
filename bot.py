2cb07d27df2281161c61f9439dc7372caf3ce56b
alfred154 <alfred@vapecity.us>
2026-03-20 11:00:45 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(TOKEN)
 
 intents = discord.Intents.default()
 intents.message_content = True  # REQUIRED for reading messages
